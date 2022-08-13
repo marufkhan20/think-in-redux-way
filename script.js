041ee249ee25f32eea0a2fdf7215b375a7a8d785
marufkhan20 <mdm326040@gmail.com>
2022-08-13 11:18:36 +0600

    Create Counter App Using Redux

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const counterEl = document.getElementById("counter");
 const incrementEl = document.getElementById("increment");
 const decrementEl = document.getElementById("decrement");
 
+// action identifiers
+const INCREMENT = "increment";
+const DECREMENT = "decrement";
+
+// action creators
+const increment = (value) => {
+  return {
+    type: INCREMENT,
+    payload: value,
+  };
+};
+
+const decrement = (value) => {
+  return {
+    type: DECREMENT,
+    payload: value,
+  };
+};
+
 // initial state
 const initialState = {
   value: 0,
@@ -10,15 +29,15 @@ const initialState = {
 
 // create reducer function
 const counterReducer = (state = initialState, action) => {
-  if (action.type === "increment") {
+  if (action.type === INCREMENT) {
     return {
       ...state,
-      value: state.value + 1,
+      value: state.value + action.payload,
     };
-  } else if (action.type === "decrement") {
+  } else if (action.type === DECREMENT) {
     return {
       ...state,
-      value: state.value - 1,
+      value: state.value - action.payload,
     };
   } else {
     return state;
@@ -38,15 +57,11 @@ render();
 
 store.subscribe(render);
 
-// listening button action
+// add event listener
 incrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "increment",
-  });
+  store.dispatch(increment(5));
 });
 
 decrementEl.addEventListener("click", () => {
-  store.dispatch({
-    type: "decrement",
-  });
+  store.dispatch(decrement(2));
 });
